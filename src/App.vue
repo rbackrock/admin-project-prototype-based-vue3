d505e4cf046ba03ecc796940ef14582bf2b056b9
rbackrock <cokekokotea@gmail.com>
2022-03-10 15:10:08 +0800

    # update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <script setup>
-  import { ref } from 'vue'
-  import zhCN from 'ant-design-vue/es/locale/zh_CN'
-  import dayjs from 'dayjs';
-  import 'dayjs/locale/zh.js'
-  dayjs.locale('zh')
-  
-  const locale = zhCN
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh.js'
+
+dayjs.locale('zh')
+
+const locale = zhCN
 </script>
 
 <template>
   <div id="app-container">
     <a-config-provider :locale="locale">
-      <router-view/>
+      <router-view />
     </a-config-provider>
   </div>
 </template>
