40e8bb09b3c4fdbca928d625e38792470af0634e
rbackrock <cokekokotea@gmail.com>
2022-03-10 15:58:22 +0800

    # 添加 jsx 能力

--- vite.config.js
+++ vite.config.js
@@ -3,23 +3,25 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 const REQUEST_BASE = 'http://localhost:8100'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '/@': resolve(__dirname, 'src')
-    }
-  },
   plugins: [
     vue(),
+    vueJsx(),
     Components({
       resolvers: [
         AntDesignVueResolver(),
       ],
     })
   ],
+  resolve: {
+    alias: {
+      '/@': resolve(__dirname, 'src')
+    }
+  },
   css: {
     preprocessorOptions: {
       less: {
