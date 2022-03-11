b0651bd1b4115b515802e2746925214060a4a1d0
rbackrock <cokekokotea@gmail.com>
2022-03-11 16:48:59 +0800

    # cleanup code

--- src/views/features/404.vue
+++ src/views/features/404.vue
@@ -1,7 +1,16 @@
 <template>
-  <a-result status="404" title="404" sub-title="对不起，没有此页面">
+  <a-result
+    status="404"
+    title="404"
+    sub-title="对不起，没有此页面"
+  >
     <template #extra>
-      <a-button type="primary" @click="handleBackHome">回到首页</a-button>
+      <a-button
+        type="primary"
+        @click="handleBackHome"
+      >
+        回到首页
+      </a-button>
     </template>
   </a-result>
 </template>
