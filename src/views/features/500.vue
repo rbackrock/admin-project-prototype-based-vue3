ec1946065601f816756d440651a760be5f4b271b
rbackrock <cokekokotea@gmail.com>
2022-03-11 16:37:54 +0800

    # 完善异常页面

--- src/views/features/500.vue
+++ src/views/features/500.vue
@@ -1,7 +1,10 @@
 <template>
-  <a-result status="500" title="500" sub-title="对不起，服务器错误，请您联系管理员">
-    <template #extra>
-    </template>
+  <a-result
+    status="500"
+    title="500"
+    sub-title="对不起，服务器错误，请您联系管理员"
+  >
+    <template #extra />
   </a-result>
 </template>
 
