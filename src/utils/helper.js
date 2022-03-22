be4d5a2d3ebed62ff03d774cb2ed07136f68cfcd
rbackrock <cokekokotea@gmail.com>
2022-03-22 17:08:36 +0800

    # 节流和防抖提供访问各自对象的能力

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -4,6 +4,7 @@ import _ from 'lodash'
  * 在组件使用中，需要使用节流，用这个方法来包裹事件即可，不要使用在复用组件中，详情见 Vue 官方文档
  * https://v3.cn.vuejs.org/guide/data-methods.html#%E9%98%B2%E6%8A%96%E5%92%8C%E8%8A%82%E6%B5%81
  * @param {Function} cb 需要使用节流的函数
+ * @param {Function} ctx 设置回调函数中的 this 的指向
  * @param {Number} millisecond 节流间隔毫秒数
  * @param {Object} opts lodash 中节流的参数
  * @returns 节流事件函数
@@ -11,14 +12,19 @@ import _ from 'lodash'
 export const throttle = (cb, ctx = undefined, millisecond = 3000, opts = {
   leading: true,
   trailing: false
-}) => _.throttle(function (...args) {
-  cb && cb.call(ctx ? ctx : this, ...args)
-}, millisecond, opts)
+}) => {
+  const throttleInstance = _.throttle(function (...args) {
+    cb && cb.call(ctx ? ctx : this, throttleInstance, ...args)
+  }, millisecond, opts)
+
+  return throttleInstance
+}
 
 /**
  * 在组件使用中，需要使用防抖，用这个方法来包裹事件即可，不要使用在复用组件中，详情见 Vue 官方文档
  * https://v3.cn.vuejs.org/guide/data-methods.html#%E9%98%B2%E6%8A%96%E5%92%8C%E8%8A%82%E6%B5%81
  * @param {Function} cb 需要使用防抖的函数
+ * @param {Function} ctx 设置回调函数中的 this 的指向
  * @param {Number} millisecond 防抖间隔毫秒数
  * @param {Object} opts lodash 中防抖的参数
  * @returns 防抖事件函数
@@ -26,9 +32,13 @@ export const throttle = (cb, ctx = undefined, millisecond = 3000, opts = {
 export const debounce = (cb, ctx = undefined, millisecond = 300, opts = {
   leading: true,
   trailing: false
-}) => _.debounce(function (...args) {
-  cb && cb.call(ctx ? ctx : this, ...args)
-}, millisecond, opts)
+}) => {
+  const debounceInstance = _.debounce(function (...args) {
+    cb && cb.call(ctx ? ctx : this, debounceInstance, ...args)
+  }, millisecond, opts)
+
+  return debounceInstance
+}
 
 /**
  * 获取树结构指定属性的一维数组，支持数组和对象，如果传入唯一属性，返回的结果则是对象
