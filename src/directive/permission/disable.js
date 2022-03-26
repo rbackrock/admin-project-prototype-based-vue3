183c3a2a6e536dab1f0b9f444bf7c5d2cca5e711
rbackrock <cokekokotea@gmail.com>
2022-03-26 22:05:15 +0800

    # 修改权限自定义指令的判断逻辑，支持当前路由匹配和全字符串匹配

--- src/directive/permission/disable.js
+++ src/directive/permission/disable.js
@@ -1,6 +1,23 @@
 import { useUserStore } from '/@/store/user'
 import _ from 'lodash'
 
+function validPermission(value, currentRouteName, currentUserRule) {
+  // 就算用了权限指令，也没有值，判断为有权限
+  if (!value || value.length === 0) {
+    return true
+  }
+
+  // 有值，那么和用户 role 数据进行对比判断有没有权限
+  for (let i = 0; i < value.length; i++) {
+    const currentPermission = value[i]
+    if (currentUserRule.indexOf(`${currentRouteName}:${currentPermission}`) !== -1 || currentUserRule.indexOf(currentPermission) !== -1) {
+      return true
+    }
+  }
+
+  return false
+}
+
 function permission(el, binding, vnode) {
   const {
     value,
@@ -12,11 +29,7 @@ function permission(el, binding, vnode) {
     const userStore = useUserStore()
     const currentUserRule = userStore.userRule
     const currentRouteName = vm.$route.name
-    let hasPermission = false
-
-    if (value.every((item) => currentUserRule.indexOf(`${currentRouteName}:${item}`) !== -1)) {
-      hasPermission = true
-    }
+    let hasPermission = validPermission(value, currentRouteName, currentUserRule)
 
     if (hasPermission === false) {
       // disable
